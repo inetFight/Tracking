0b31f9adc92c539442bf054a6d769082a72e4f72
litvinov.do <d-litvinov@ukr.net>
2017-02-21 13:38:09 +0200

    reload new logic

--- src/main/java/NPI/Services/NewLogicAndWriteFile.java
+++ src/main/java/NPI/Services/NewLogicAndWriteFile.java
@@ -18,106 +18,134 @@ public class NewLogicAndWriteFile {
 		for(CompareModel obj: list){
 			String toWrite = obj.getFirstNumberEW() + ";"
 						   + obj.getFirstStatusEW() + ";"
+						   + obj.getFirstStatusCodeEw() + ";"
 						   + obj.getFirstRecipientDateTime();;
 			//Проверяем 4 стадию
-			if(obj.getFourthBasicTypeEW().equals("") == false){
-				if(obj.getFourthBasicTypeEW().equals("CargoReturn") && obj.getFourthStatusCodeEw().equals("9")){
+			if(obj.getFourthStatusCodeEw().equals("") == false){
+				if(obj.getThirdBasicTypeEW().equals("CargoReturn") && obj.getFourthStatusCodeEw().equals("9")){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат принят ЦСС" + ";"
+							+ obj.getFourthStatusCodeEw() + ";"
 							+ obj.getFourthRecipientDateTime();
 				}
-				else if(obj.getFourthBasicTypeEW().equals("CargoReturn") && obj.getFourthStatusEW().equals("9") == false){
+				else if(obj.getThirdBasicTypeEW().equals("CargoReturn") && obj.getFourthStatusCodeEw().equals("9") == false){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат на пути на ЦСС" + ";"
+							+ obj.getFourthStatusCodeEw() + ";"
 							+ obj.getFourthRecipientDateTime();
 				}
-				else if(obj.getFourthBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getFourthStatusCodeEw().equals("9")){
+				else if(obj.getThirdBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getFourthStatusCodeEw().equals("9")){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат принят Склад Полтава" + ";"
+							+ obj.getFourthStatusCodeEw() + ";"
 							+ obj.getFourthRecipientDateTime();
 					
 				}
-				else if(obj.getFourthBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getFourthStatusCodeEw().equals("9") == false){
+				else if(obj.getThirdBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getFourthStatusCodeEw().equals("9") == false){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат на пути на Склад Полтава" + ";"
+							+ obj.getFourthStatusCodeEw() + ";"
 							+ obj.getFourthRecipientDateTime();
 								
 				}
 				else {
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ obj.getFourthStatusEW() + ";"
+							+ obj.getFourthStatusCodeEw() + ";"
 							+ obj.getFourthRecipientDateTime();
 				}
+				writerResultShort.write(toWrite);
+				writerResultShort.write(System.lineSeparator());
+				writerResultShort.flush();
+				continue;
 				
 			}
 			//Проверяем 3 стадию
-			if(obj.getThirdBasicTypeEW().equals("") == false){
-				if(obj.getThirdBasicTypeEW().equals("CargoReturn") && obj.getThirdStatusCodeEw().equals("9")){
+			if(obj.getThirdStatusCodeEw().equals("") == false){
+				if(obj.getSecondBasicTypeEW().equals("CargoReturn") && obj.getThirdStatusCodeEw().equals("9")){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат принят ЦСС" + ";"
+							+ obj.getThirdStatusCodeEw() + ";"
 							+ obj.getThirdRecipientDateTime();
 				}
-				else if(obj.getThirdBasicTypeEW().equals("CargoReturn") && obj.getThirdStatusEW().equals("9") == false){
+				else if(obj.getSecondBasicTypeEW().equals("CargoReturn") && obj.getThirdStatusCodeEw().equals("9") == false){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат на пути на ЦСС" + ";"
+							+ obj.getThirdStatusCodeEw() + ";"
 							+ obj.getThirdRecipientDateTime();
 				}
-				else if(obj.getThirdBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getThirdStatusCodeEw().equals("9")){
+				else if(obj.getSecondBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getThirdStatusCodeEw().equals("9")){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат принят Склад Полтава" + ";"
+							+ obj.getThirdStatusCodeEw() + ";"
 							+ obj.getThirdRecipientDateTime();
 					
 				}
-				else if(obj.getThirdBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getThirdStatusCodeEw().equals("9") == false){
+				else if(obj.getSecondBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getThirdStatusCodeEw().equals("9") == false){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат на пути на Склад Полтава" + ";"
+							+ obj.getThirdStatusCodeEw() + ";"
 							+ obj.getThirdRecipientDateTime();
 								
 				}
 				else {
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ obj.getThirdStatusEW() + ";"
+							+ obj.getThirdStatusCodeEw() + ";"
 							+ obj.getThirdRecipientDateTime();
 				}
+				writerResultShort.write(toWrite);
+				writerResultShort.write(System.lineSeparator());
+				writerResultShort.flush();
+				continue;
 				
 			}
-			
-			//Проверяем 2 стадию
-			if(obj.getSecondBasicTypeEW().equals("") == false){
-				if(obj.getSecondBasicTypeEW().equals("CargoReturn") && obj.getSecondStatusCodeEw().equals("9")){
+			//проверяем вторую стадию
+			if(obj.getSecondStatusCodeEw().equals("") == false){
+				if(obj.getFirstBasicTypeEW().equals("CargoReturn") && obj.getSecondStatusCodeEw().equals("9")){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат принят ЦСС" + ";"
+							+ obj.getSecondStatusCodeEw() + ";"
 							+ obj.getSecondRecipientDateTime();
 				}
-				else if(obj.getSecondBasicTypeEW().equals("CargoReturn") && obj.getSecondStatusEW().equals("9") == false){
+				else if(obj.getFirstBasicTypeEW().equals("CargoReturn") && obj.getSecondStatusCodeEw().equals("9") == false){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат на пути на ЦСС" + ";"
+							+ obj.getSecondStatusCodeEw() + ";"
 							+ obj.getSecondRecipientDateTime();
 				}
-				else if(obj.getSecondBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getSecondStatusCodeEw().equals("9")){
+				else if(obj.getFirstBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getSecondStatusCodeEw().equals("9")){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат принят Склад Полтава" + ";"
+							+ obj.getSecondStatusCodeEw() + ";"
 							+ obj.getSecondRecipientDateTime();
 					
 				}
-				else if(obj.getSecondBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getSecondStatusCodeEw().equals("9") == false){
+				else if(obj.getFirstBasicTypeEW().equals("ReturnToWarehousePoltava") && obj.getSecondStatusCodeEw().equals("9") == false){
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ "Возврат на пути на Склад Полтава" + ";"
+							+ obj.getSecondStatusCodeEw() + ";"
 							+ obj.getSecondRecipientDateTime();
 								
 				}
 				else {
 					toWrite = obj.getFirstNumberEW() + ";"
 							+ obj.getSecondStatusEW() + ";"
+							+ obj.getSecondStatusCodeEw() + ";"
 							+ obj.getSecondRecipientDateTime();
 				}
+				writerResultShort.write(toWrite);
+				writerResultShort.write(System.lineSeparator());
+				writerResultShort.flush();
+				continue;
 				
 			}
-			
 			
 			writerResultShort.write(toWrite);
 			writerResultShort.write(System.lineSeparator());
 			writerResultShort.flush();
+			
+			
 			
 			
 			
